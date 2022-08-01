307f232e0640ea92ee93d0d001a1e8bd962376e4
zamonosman <zamonosman@localhost>
2022-08-01 19:42:27 -0400

    attempt2

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -5,10 +5,12 @@ public class DemoTest {
 
 	
 		
-		public static void add(int a, int b) {
-			System.out.println(a + b);
-		}
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
 
-		
+	public static void divid(int a, int b) {
+		System.out.println(a / b);
+	}
 	
 }
