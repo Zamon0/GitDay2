3cb4abb20260f57df450ccac614d82c3b83eca34
zamonosman <zamonosman@localhost>
2022-08-01 19:44:18 -0400

    test

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -3,7 +3,12 @@ package demo;
 
 public class DemoTest {
 
+	public static void main(String[] args) {
+		
 	
+	System.out.println("Testing");
+	
+}
 		
 	public static void add(int a, int b) {
 		System.out.println(a + b);
