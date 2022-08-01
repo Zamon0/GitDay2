bb4a3c937f59207d0b1f30985e2ba353bbf1f6e6
zamonosman <zamonosman@localhost>
2022-08-01 19:31:33 -0400

    run

--- src/test2/Run2.java
+++ src/test2/Run2.java
@@ -2,8 +2,12 @@ package test2;
 
 public class Run2 {
 	
-	public static void main(String[] args) {
-		System.out.println("Run");
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 	}
 
+	
 }
+	
+
+
